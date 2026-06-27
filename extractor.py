7e289b007ab33b21f14179345b4d5e9310969493
Amir Mohammad <amirmohammadshir1386@gmail.com>
2026-06-28 01:12:00 +0330

    fix hashtag function and optimizing other code and add comments

--- extractor.py
+++ extractor.py
@@ -1,6 +1,14 @@
 import cleaner
-
+import regex as re
 def parse_csv(path: str):
     with open(path, 'r', encoding='utf-8', errors='ignore') as csv:
-        for row in csv:
-            yield row.strip().split(',')
+        for line in csv:
+            line = line.strip()
+            line = re.sub(r'\"\d+-\d+-\d+ \d+:\d+:\d+\",', '', line)
+            line = re.sub(r',\d+,', '', line)
+            yield line
+
+with open('extracted.csv', 'w', encoding='utf-8', errors='ignore') as csv:
+    for row in parse_csv("tweets.csv"):
+        row = ''.join(row)
+        csv.write(row)
